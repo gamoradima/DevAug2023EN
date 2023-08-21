f70b15fb020ff5b4fddd013a6d0f8b1695707c47
Dmitriy Gamora <gamoradima@gmail.com>
2023-08-21 19:15:37 +0300

    Classic UI calculations, BP signals event handling

---
+++ ClassicUIExamples/Schemas/UsrRealtyClassic1Page/UsrRealtyClassic1Page.js
@@ -0,0 +1,359 @@
+define("UsrRealtyClassic1Page", [], function() {
+	return {
+		entitySchemaName: "UsrRealtyClassic",
+		attributes: {
+			"UsrCommission": {
+				dependencies: [
+					{
+						columns: ["UsrPriceUSD", "UsrOfferType"],
+						methodName: "calculateCommission"
+					}
+				],
+				"caption": "Commission, USD"
+			},
+			"UsrOfferType": {
+				lookupListConfig: {
+					columns: ["UsrPercent"]
+				}
+			}
+		},
+		modules: /**SCHEMA_MODULES*/{}/**SCHEMA_MODULES*/,
+		details: /**SCHEMA_DETAILS*/{
+			"Files": {
+				"schemaName": "FileDetailV2",
+				"entitySchemaName": "UsrRealtyClassicFile",
+				"filter": {
+					"masterColumn": "Id",
+					"detailColumn": "UsrRealtyClassic"
+				}
+			}
+		}/**SCHEMA_DETAILS*/,
+		businessRules: /**SCHEMA_BUSINESS_RULES*/{
+			"UsrComment": {
+				"789a83f0-2e39-417a-9d34-a1cc21497cab": {
+					"uId": "789a83f0-2e39-417a-9d34-a1cc21497cab",
+					"enabled": true,
+					"removed": false,
+					"ruleType": 0,
+					"property": 2,
+					"logical": 0,
+					"conditions": [
+						{
+							"comparisonType": 7,
+							"leftExpression": {
+								"type": 1,
+								"attribute": "UsrPriceUSD"
+							},
+							"rightExpression": {
+								"type": 0,
+								"value": 1000,
+								"dataValueType": 5
+							}
+						}
+					]
+				}
+			}
+		}/**SCHEMA_BUSINESS_RULES*/,
+		methods: {
+			calculateCommission: function() {
+				var price = this.get("UsrPriceUSD");
+				if (!price) {
+					price = 0;
+				}
+				var offerTypeObject = this.get("UsrOfferType");
+				var percent = 0;
+				if (offerTypeObject) {
+					percent = offerTypeObject.UsrPercent;
+				}
+				var commission = price * percent / 100;
+				this.set("UsrCommission", commission);
+				this.console.log("Commission = " + commission);
+			},
+
+			onEntityInitialized: function() {
+				this.callParent(arguments);
+				this.calculateCommission();
+			},
+
+			onMyButtonClick: function() {
+				this.console.log("PUSH IT!");
+				var price = this.get("UsrPriceUSD");
+				this.console.log("Price = " + price);
+				this.set("UsrArea", 2000);
+			},
+			getMyButtonEnabled: function() {
+				var result = true;
+				if (!this.get("UsrName")) {
+					result = false;
+				}
+				return result;
+			},
+			
+			positiveValueValidator: function(value, column) {
+				var msg = "";
+				if (value < 0) {
+					msg = this.get("Resources.Strings.ValueMustBeGreaterThanZero");
+				}
+				return {
+					invalidMessage: msg
+				};
+			},
+			setValidationConfig: function() {
+				this.callParent(arguments);
+				this.addColumnValidator("UsrPriceUSD", this.positiveValueValidator);
+				this.addColumnValidator("UsrArea", this.positiveValueValidator);
+			}
+		},
+		dataModels: /**SCHEMA_DATA_MODELS*/{}/**SCHEMA_DATA_MODELS*/,
+		diff: /**SCHEMA_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "UsrName5266b946-06dd-4e4e-8146-5b9aaadd4e1b",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrName",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "FLOAT300faade-d359-4871-aea6-7f7a2041418a",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 1,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrPriceUSD",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "FLOAT4c090165-585a-4eaa-ba62-74888a0c94c9",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 2,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrArea",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "FLOAT1f3fdb1f-6053-46de-9487-e92b798f1b76",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 3,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrCommission",
+					"enabled": false
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 3
+			},
+			{
+				"operation": "insert",
+				"name": "PrimaryContactButton",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 4,
+						"layoutName": "ProfileContainer"
+					},
+					"itemType": 5,
+					"caption": {
+						"bindTo": "Resources.Strings.MyButtonCaption"
+					},
+					"click": {
+						"bindTo": "onMyButtonClick"
+					},
+					"enabled": {
+						"bindTo": "getMyButtonEnabled"
+					},
+					"style": "blue"
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 4
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUP93ff15b0-c1ec-41bc-af42-e82aa39cd49d",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrType",
+					"enabled": true,
+					"contentType": 3
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUPdb551704-d3cc-49d5-b7c1-20af79b865f3",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrOfferType",
+					"enabled": true,
+					"contentType": 3
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "STRING0ff60c7e-75c3-431f-bcdc-275f17932867",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 1,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrComment",
+					"enabled": true,
+					"contentType": 0
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUPcd3bdc48-7fd4-47db-9c7f-172cfe4b6c74",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 1,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrManager",
+					"enabled": true,
+					"contentType": 5
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 3
+			},
+			{
+				"operation": "insert",
+				"name": "NotesAndFilesTab",
+				"values": {
+					"caption": {
+						"bindTo": "Resources.Strings.NotesAndFilesTabCaption"
+					},
+					"items": [],
+					"order": 0
+				},
+				"parentName": "Tabs",
+				"propertyName": "tabs",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Files",
+				"values": {
+					"itemType": 2
+				},
+				"parentName": "NotesAndFilesTab",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "NotesControlGroup",
+				"values": {
+					"itemType": 15,
+					"caption": {
+						"bindTo": "Resources.Strings.NotesGroupCaption"
+					},
+					"items": []
+				},
+				"parentName": "NotesAndFilesTab",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "Notes",
+				"values": {
+					"bindTo": "UsrNotes",
+					"dataValueType": 1,
+					"contentType": 4,
+					"layout": {
+						"column": 0,
+						"row": 0,
+						"colSpan": 24
+					},
+					"labelConfig": {
+						"visible": false
+					},
+					"controlConfig": {
+						"imageLoaded": {
+							"bindTo": "insertImagesToNotes"
+						},
+						"images": {
+							"bindTo": "NotesImagesCollection"
+						}
+					}
+				},
+				"parentName": "NotesControlGroup",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "merge",
+				"name": "ESNTab",
+				"values": {
+					"order": 1
+				}
+			}
+		]/**SCHEMA_DIFF*/
+	};
+});
